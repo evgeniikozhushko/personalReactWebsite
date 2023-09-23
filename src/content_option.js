f5521e4e6bc9c9d76d92dd35ea28a52bb8ac6e12
Evgenii Kozhushko <climbersrock.eugene@gmail.com>
2023-09-22 23:26:43 -0600

    Portfolio tweak, div edit.

--- src/content_option.js
+++ src/content_option.js
@@ -78,7 +78,7 @@ const services = [{
 ];
 
 const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
+        img: "",
         description: "Bow Valley Climbing Access Society (BVCAS).",
         link: "#",
     },
@@ -107,7 +107,6 @@ const dataportfolio = [{
         description: "",
         link: "#",
     },
-
     {
         img: "",
         description: "",
