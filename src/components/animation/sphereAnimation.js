e3d7b5cd9180609a61498ffe9fdb831e3a508fa8
Evgenii Kozhushko <climbersrock.eugene@gmail.com>
2023-09-24 23:28:27 -0600

    Animation integration.

---
+++ src/components/animation/sphereAnimation.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SphereAnimation extends Component {
+render() {
+    return (
+        <div className="animation-wrapper">
+            <div className="sphere-animation">
+            {/* Paste the SVG code here */}
+            </div>
+        </div>
+        );
+    } 
+}
+export default SphereAnimation;
